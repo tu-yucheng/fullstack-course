2b6366cd9fc5829be9b8296904bdd661b684f9c8
tuyucheng <tuyucheng2000@163.com>
2022-10-30 15:08:43 +0800

    How to Convert Mono<List<T>> Into Flux<T>

--- reactive-programming/reactor-core/src/test/java/cn/tuyucheng/taketoday/reactor/mono/MonoUnitTest.java
+++ reactive-programming/reactor-core/src/test/java/cn/tuyucheng/taketoday/reactor/mono/MonoUnitTest.java
@@ -2,10 +2,14 @@ package cn.tuyucheng.taketoday.reactor.mono;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -38,5 +42,40 @@ class MonoUnitTest {
 
 		blockingHelloWorld()
 				.subscribe(result -> assertEquals("Hello world!", result));
+	}
+
+	@Test
+	void whenMonoProducesListOfElements_thenConvertToFluxOfElements() {
+		Mono<List<String>> monoList = monoOfList();
+
+		StepVerifier.create(monoToFluxUsingFlatMapIterable(monoList))
+				.expectNext("one", "two", "three", "four")
+				.verifyComplete();
+
+		StepVerifier.create(monoToFluxUsingFlatMapMany(monoList))
+				.expectNext("one", "two", "three", "four")
+				.verifyComplete();
+	}
+
+	private Mono<List<String>> monoOfList() {
+		List<String> list = new ArrayList<>();
+		list.add("one");
+		list.add("two");
+		list.add("three");
+		list.add("four");
+
+		return Mono.just(list);
+	}
+
+	private <T> Flux<T> monoToFluxUsingFlatMapMany(Mono<List<T>> monoList) {
+		return monoList
+				.flatMapMany(Flux::fromIterable)
+				.log();
+	}
+
+	private <T> Flux<T> monoToFluxUsingFlatMapIterable(Mono<List<T>> monoList) {
+		return monoList
+				.flatMapIterable(list -> list)
+				.log();
 	}
 }
