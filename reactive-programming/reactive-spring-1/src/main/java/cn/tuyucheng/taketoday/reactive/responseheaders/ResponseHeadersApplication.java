78d0e6d9156a5f0bdfe827ed155714adbd02fc7d
tuyucheng <tuyucheng2000@163.com>
2022-10-31 18:30:25 +0800

    How to Set a Header on a Response with Spring 5

---
+++ reactive-programming/reactive-spring-1/src/main/java/cn/tuyucheng/taketoday/reactive/responseheaders/ResponseHeadersApplication.java
@@ -0,0 +1,17 @@
+package cn.tuyucheng.taketoday.reactive.responseheaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class, SecurityAutoConfiguration.class, ReactiveSecurityAutoConfiguration.class})
+public class ResponseHeadersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResponseHeadersApplication.class, args);
+    }
+}
