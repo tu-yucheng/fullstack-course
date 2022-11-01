f620f7e0a8277f3691b57e15c54ff32c19d22219
tuyucheng <tuyucheng2000@163.com>
2022-11-01 17:20:51 +0800

    update feature file location

--- software-test/cucumber/src/test/java/cn/tuyucheng/taketoday/cucumber/runner/RunCucumberWithJUnit4Test.java
+++ software-test/cucumber/src/test/java/cn/tuyucheng/taketoday/cucumber/runner/RunCucumberWithJUnit4Test.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "source/testing/resources/cn/tuyucheng/taketoday/cucumber/features",
+		features = "src/test/resources/cn/tuyucheng/taketoday/cucumber/features",
 		glue = {"cn/tuyucheng/taketoday/cucumber/steps", "cn/tuyucheng/taketoday/cucumber/e2e/steps"},
 		tags = "@End2EndIntegrationTest"
 )
